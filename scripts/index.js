b39309d38a5d794bcee234de04c626bd8c4af3aa
Samir Cunha <samircunha98@gmail.com>
2022-01-06 13:30:19 -0300

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,72 @@
+class CalculationArea {
+  constructor(type, material, value, length, amount) {
+    (this.type = type),
+      (this.material = material),
+      (this.value = value),
+      (this.length = length),
+      (this.amount = amount);
+  }
+
+  metals = [
+    { material: "aluminio", value: 2.71 / 1000 },
+    { material: "bronze", value: 8.7 / 1000 },
+    { material: "cobre", value: 8.93 / 1000 },
+    { material: "latao", value: 8.7 / 1000 },
+  ];
+
+  pi = 3.14;
+
+  materialWeightWithSquareBar() {
+    const squareArea = Math.pow(this.value / 10, 2) * (this.length * 100);
+    const takeMaterialWeight = this.metals.filter((metal) => {
+      if (metal.material == this.material) {
+        return metal;
+      }
+      return;
+    });
+    const barWeight = squareArea * takeMaterialWeight[0].value * this.amount;
+    return (Math.round(barWeight));
+  }
+
+  materialWeightWithRoundBar() {
+    const roundArea =
+      Math.pow(this.value / 10 / 2, 2) * (this.length * 100) * this.pi;
+    const takeMaterialWeight = this.metals.filter((metal) => {
+      if (metal.material == this.material) {
+        return metal;
+      }
+      return;
+    });
+    const barWeight = roundArea * takeMaterialWeight[0].value * this.amount;
+    return (Math.round(barWeight));
+  }
+}
+
+function handleCalculationWeight(bar) {
+  const material = document.querySelector("." + bar + "-material").value;
+  const value = document.querySelector("." + bar + "-value").value;
+  const length = Number(document.querySelector("." + bar + "-length").value);
+  const amount = Number(document.querySelector("." + bar + "-amount").value);
+
+  const newBar = new CalculationArea(bar, material, value, length, amount);
+
+  switch (bar) {
+    case "square":
+      document
+        .getElementById(`show-result-${bar}`)
+        .innerHTML = `Peso total: ${newBar.materialWeightWithSquareBar()} Kg`;
+      break;
+    case "round":
+        document
+        .getElementById(`show-result-${bar}`)
+        .innerHTML = `Peso total: ${newBar.materialWeightWithRoundBar()} Kg`;
+      break;
+    default:
+      alert("Erro ao Calcular os Quantitativos");
+      break;
+  }
+}
+
+function handleShowCalculationContainer(attribute) {
+  document.querySelector("." + attribute).toggleAttribute("hidden");
+}
