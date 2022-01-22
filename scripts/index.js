9bc45e1c48323135222c75e895cd7760ebc7b847
Samir Cunha <samircunha98@gmail.com>
2022-01-22 14:28:49 -0300

    ajuste de styles, acréscimo da imagem, ajuste no script, tirei por enquanto a animação

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,18 @@
-let materialSelected = '';
-let materialButtonSelected = '';
+let materialSelected = 'aluminio';
+let materialButtonSelected = {button: document.querySelector(".button-aluminio"), type: "aluminio"};
 let checkOpenedNav = {status: 'close', openedNav: {classNavOpened: '', barTypeOpened: ''}};
+const metals = [
+  { material: "aluminio", value: 2.70 / 1000 },
+  { material: "bronze", value: 8.7 / 1000 },
+  { material: "cobre", value: 8.93 / 1000 },
+  { material: "latao", value: 8.7 / 1000 },
+  { material: "acoinox", value: 7.9 / 1000 },
+];
+const pi = 3.14;
+
+window.addEventListener("DOMContentLoaded", ()=>{
+  materialButtonSelected.button.setAttribute('class', `material-button__buttons button-aluminio button-aluminio-active`);
+})
 
 class CalculationArea {
   constructor(type, material, value) {
@@ -9,19 +21,9 @@ class CalculationArea {
       (this.value = value)
   }
 
-  metals = [
-    { material: "aluminio", value: 2.70 / 1000 },
-    { material: "bronze", value: 8.7 / 1000 },
-    { material: "cobre", value: 8.93 / 1000 },
-    { material: "latao", value: 8.7 / 1000 },
-    { material: "acoinox", value: 7.9 / 1000 },
-  ];
-
-  pi = 3.14;
-
   materialWeightWithSquareBar() {
     const squareArea = Math.pow(this.value / 10, 2) * 100;
-    const takeMaterialWeight = this.metals.filter((metal) => {
+    const takeMaterialWeight = metals.filter((metal) => {
       if (metal.material == this.material) {
         return metal;
       }
@@ -32,9 +34,8 @@ class CalculationArea {
   }
 
   materialWeightWithRoundBar() {
-    const roundArea =
-      Math.pow(this.value / 10 / 2, 2) * 100 * this.pi;
-    const takeMaterialWeight = this.metals.filter((metal) => {
+    const roundArea = Math.pow(this.value / 10 / 2, 2) * 100 * pi;
+    const takeMaterialWeight = metals.filter((metal) => {
       if (metal.material == this.material) {
         return metal;
       }
@@ -52,12 +53,12 @@ function handleCalculationWeight(bar) {
   const newBar = new CalculationArea(bar, material, value);
 
   switch (bar) {
-    case "square":
+    case "square-bar":
       document
         .getElementById(`show-result-${bar}`)
         .innerHTML = `<b>Peso total: ${newBar.materialWeightWithSquareBar()} Kg</b>`;
       break;
-    case "round":
+    case "round-bar":
         document
         .getElementById(`show-result-${bar}`)
         .innerHTML = `<b>Peso total: ${newBar.materialWeightWithRoundBar()} Kg</b>`;
@@ -65,37 +66,6 @@ function handleCalculationWeight(bar) {
     default:
       alert("Erro ao Calcular os Quantitativos");
       break;
-  }
-}
-
-function handleShowCalculationContainer({calulationContainerBar, barType}) {
-  toggleNavBar(calulationContainerBar, barType);
-}
-
-function toggleHidden (classCalculationContainerBar, time) {
-  setTimeout(()=>{
-    document.querySelector("." + classCalculationContainerBar).toggleAttribute("hidden");
-  }, time)
-}
-
-function toggleNavBar (classCalculationContainerBar, barType) {
-  if(checkOpenedNav.status == 'close'){
-    console.log(true)
-    document.querySelector(`.${barType}`).setAttribute("class", `${barType} open-nav-bars`);
-    toggleHidden (classCalculationContainerBar, 300);
-    checkOpenedNav = {status: 'open', openedNav: {classNavOpened: classCalculationContainerBar, barTypeOpened: barType}};
-  } else {
-    if(checkOpenedNav.openedNav.barTypeOpened == barType){
-      document.querySelector(`.${barType}`).setAttribute("class", `${barType} close-nav-bars`);
-      toggleHidden (classCalculationContainerBar, 0);
-      checkOpenedNav.status = 'close';
-    } else {
-      document.querySelector(`.${checkOpenedNav.openedNav.barTypeOpened}`).setAttribute("class", `${checkOpenedNav.openedNav.barTypeOpened} close-nav-bars`);
-      toggleHidden (checkOpenedNav.openedNav.classNavOpened, 0);
-      document.querySelector(`.${barType}`).setAttribute("class", `${barType} open-nav-bars`);
-      toggleHidden (classCalculationContainerBar, 300);
-      checkOpenedNav.openedNav = {classNavOpened: classCalculationContainerBar, barTypeOpened: barType}
-    }
   }
 }
 
@@ -107,3 +77,49 @@ function handleSelectMaterial (material) {
   materialButtonSelected = {button: document.querySelector(".button-" + material), type: material};
   materialButtonSelected.button.setAttribute('class', `material-button__buttons button-${material} button-${material}-active`);
 }
+
+function handleShowCalculationContainer({calulationContainerBar, barType}) {
+  toggleNavBar(calulationContainerBar, barType);
+}
+
+function toggleNavBar (classCalculationContainerBar, barType) {
+  if(checkOpenedNav.status == 'close'){
+    document.querySelector("." + classCalculationContainerBar).setAttribute("class", `product-structure__body ${barType}__body`);
+    checkOpenedNav = {status: 'open', openedNav: {classNavOpened: classCalculationContainerBar, barTypeOpened: barType}};
+  } else {
+    if(checkOpenedNav.openedNav.barTypeOpened == barType){
+      document.querySelector("." + classCalculationContainerBar).setAttribute("class", `product-structure__body ${barType}__body hidden-product-body`);
+      checkOpenedNav.status = 'close';
+    } else {
+      document.querySelector(`.${checkOpenedNav.openedNav.classNavOpened}`).setAttribute("class", `product-structure__body ${checkOpenedNav.openedNav.barTypeOpened}__body hidden-product-body`);
+      document.querySelector(`.${classCalculationContainerBar}`).setAttribute("class", `product-structure__body ${barType}__body`);
+      checkOpenedNav.openedNav = {classNavOpened: classCalculationContainerBar, barTypeOpened: barType};
+    }
+  }
+}
+
+// function toggleNavBar (classCalculationContainerBar, barType) {
+//   if(checkOpenedNav.status == 'close'){
+//     document.querySelector(`.${barType}`).setAttribute("class", `${barType} open-nav-bars`);
+//     toggleHidden (classCalculationContainerBar, 300);
+//     checkOpenedNav = {status: 'open', openedNav: {classNavOpened: classCalculationContainerBar, barTypeOpened: barType}};
+//   } else {
+//     if(checkOpenedNav.openedNav.barTypeOpened == barType){
+//       document.querySelector(`.${barType}`).setAttribute("class", `${barType} close-nav-bars`);
+//       toggleHidden (classCalculationContainerBar, 0);
+//       checkOpenedNav.status = 'close';
+//     } else {
+//       document.querySelector(`.${checkOpenedNav.openedNav.barTypeOpened}`).setAttribute("class", `${checkOpenedNav.openedNav.barTypeOpened} close-nav-bars`);
+//       toggleHidden (checkOpenedNav.openedNav.classNavOpened, 0);
+//       document.querySelector(`.${barType}`).setAttribute("class", `${barType} open-nav-bars`);
+//       toggleHidden (classCalculationContainerBar, 300);
+//       checkOpenedNav.openedNav = {classNavOpened: classCalculationContainerBar, barTypeOpened: barType}
+//     }
+//   }
+// }
+
+// function toggleHidden (classCalculationContainerBar, time) {
+//   setTimeout(()=>{
+//     document.querySelector("." + classCalculationContainerBar).toggleAttribute("hidden");
+//   }, time)
+// }
